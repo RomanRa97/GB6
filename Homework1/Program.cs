17dfb5a9197d73f53e43492dd1eaa1f4accebd59
roman <roraf@mail.ru>
2022-09-11 15:13:41 +0500

    Homework lesson 6

---
+++ Homework1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine("Осталось ввести чисел {0}", m-i);
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (a > 0) sum += 1;
+}
+Console.WriteLine(sum);
